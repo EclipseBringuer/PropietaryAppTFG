f9b4e4dcc8d824e6259cf7c703cc89b5e6ad46c7
gabri <gabrielrl2004@gmail.com>
2024-03-26 19:25:35 +0100

    view model del login

--- app/src/main/java/com/grl/clientapptfg/login/ui/LoginViewModel.kt
+++ app/src/main/java/com/grl/clientapptfg/login/ui/LoginViewModel.kt
@@ -1,7 +1,23 @@
 package com.grl.clientapptfg.login.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.grl.clientapptfg.login.domain.LoginUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class LoginViewModel @Inject constructor() : ViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor(private val loginUseCase: LoginUseCase) : ViewModel() {
+
+    private val _text = MutableLiveData<String>()
+    val text:LiveData<String> = _text
+
+    fun getUser() {
+        viewModelScope.launch {
+            _text.value = loginUseCase("gabrielrl2004@gmail.com", "15112004").toString()
+        }
+    }
 }
