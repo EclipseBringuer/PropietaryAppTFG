61a99d9695e5d499a0eb65b3ac0bec8b69b66428
gabri <gabrielrl2004@gmail.com>
2024-04-29 17:26:26 +0200

    Clase con rutas de las campañas

---
+++ app/src/main/java/com/grl/clientapptfg/ui/components/Screens.kt
@@ -0,0 +1,10 @@
+package com.grl.clientapptfg.ui.components
+
+sealed class Screens(val route: String) {
+    data object Login: Screens("login-route")
+    data object Menu: Screens("menu-route")
+    data object Account: Screens("account-route")
+    data object Home: Screens("home-route")
+    data object Tracking: Screens("tracking-route")
+    data object Orders: Screens("orders-route")
+}
