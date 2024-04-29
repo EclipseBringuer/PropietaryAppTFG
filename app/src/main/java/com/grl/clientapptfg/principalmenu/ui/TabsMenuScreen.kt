f16d03d91f187345f0e106061dc8db269b29681a
gabri <gabrielrl2004@gmail.com>
2024-04-29 17:35:54 +0200

    Nuevas vistas con viewmodels añadidos

--- app/src/main/java/com/grl/clientapptfg/principalmenu/ui/TabsMenuScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.grl.clientapptfg.principalmenu.ui
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun TabsMenuScreen(tabsMenuViewModel: TabsMenuViewModel) {
-
-}
--- app/src/main/java/com/grl/clientapptfg/principalmenu/ui/TabsMenuViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.grl.clientapptfg.principalmenu.ui
-
-import androidx.lifecycle.ViewModel
-import javax.inject.Inject
-
-class TabsMenuViewModel @Inject constructor() : ViewModel() {
-}
