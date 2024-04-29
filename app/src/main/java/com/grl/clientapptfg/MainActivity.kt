bcf3e596eb7f382c1fcfb49360590b50ac466660
gabri <gabrielrl2004@gmail.com>
2024-04-29 17:39:35 +0200

    Cambios en la visión de la activity principal

--- app/src/main/java/com/grl/clientapptfg/MainActivity.kt
+++ app/src/main/java/com/grl/clientapptfg/MainActivity.kt
@@ -8,16 +8,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.grl.clientapptfg.login.ui.LoginScreen
-import com.grl.clientapptfg.login.ui.LoginViewModel
+import com.grl.clientapptfg.ui.screens.login.ui.LoginViewModel
+import com.grl.clientapptfg.ui.screens.principalmenu.ui.TabsMenuScreen
+import com.grl.clientapptfg.ui.screens.principalmenu.ui.TabsMenuViewModel
 import com.grl.clientapptfg.ui.theme.ClientAppTFGTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private val tabsMenuViewModel: TabsMenuViewModel by viewModels()
     private val loginViewModel: LoginViewModel by viewModels()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen(loginViewModel)
+                    TabsMenuScreen(tabsMenuViewModel = tabsMenuViewModel, loginViewModel)
                 }
             }
         }
