d4adaf979d7df58a4d9e0f77f1acb5a6267e329a
gabri <gabrielrl2004@gmail.com>
2024-04-29 21:26:52 +0200

    Colores básicos para la aplicación añadidos

--- app/src/main/java/com/grl/clientapptfg/ui/theme/Color.kt
+++ app/src/main/java/com/grl/clientapptfg/ui/theme/Color.kt
@@ -2,11 +2,11 @@ package com.grl.clientapptfg.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-val durumColor = Color(0xFF59001C)
-val GreenLink = Color(0xFF71C11B)
-val Purple40 = Color(0xFF6650a4)
+val granate = Color(0xFF59001C)
+val white = Color(0xFFFFFFFF)
+val black = Color(0xFF000000)
+val mostaza = Color(0xFFFFC300)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
