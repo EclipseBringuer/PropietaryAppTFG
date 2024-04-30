04079a090fd1df0e85d6081ec543e4fe846f9d7d
gabri <gabrielrl2004@gmail.com>
2024-04-30 16:35:25 +0200

    Nuevo cajetín para contraseñas

--- app/src/main/java/com/grl/clientapptfg/ui/components/TextFieldPersonalized.kt
+++ app/src/main/java/com/grl/clientapptfg/ui/components/TextFieldPersonalized.kt
@@ -1,7 +1,13 @@
 package com.grl.clientapptfg.ui.components
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
@@ -10,6 +16,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.grl.clientapptfg.ui.theme.PurpleGrey80
@@ -51,5 +59,60 @@ fun TextFieldPersonalized(
             disabledContainerColor = white,
             focusedIndicatorColor = mostaza
         )
+    )
+}
+
+@Composable
+fun TextFieldForPasswordPersonalized(
+    value: String,
+    changeText: (String) -> Unit,
+    modifier: Modifier,
+    imeAction: ImeAction,
+    isVisible: Boolean,
+    changeVisibility: (Boolean) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = { changeText(it) },
+        modifier = modifier,
+        shape = RoundedCornerShape(20.dp),
+        singleLine = true,
+        textStyle = TextStyle(fontSize = 20.sp, color = black),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password,
+            imeAction = imeAction
+        ),
+        placeholder = {
+            Text(
+                text = "Constraseña",
+                fontSize = 25.sp,
+                fontFamily = Util.loadFontFamilyFromAssets(),
+                color = PurpleGrey80
+            )
+        }, colors = TextFieldDefaults.colors(
+            focusedContainerColor = white,
+            unfocusedContainerColor = white,
+            disabledContainerColor = white,
+            focusedIndicatorColor = mostaza
+        ), trailingIcon = {
+            val imagen = if (isVisible) {
+                Icons.Filled.Visibility
+            } else {
+                Icons.Filled.VisibilityOff
+            }
+            IconButton(onClick = { changeVisibility(isVisible) }) {
+                Icon(
+                    imageVector = imagen,
+                    contentDescription = "Icono de visibilidad",
+                    Modifier.size(35.dp),
+                    tint = mostaza
+                )
+            }
+        },
+        visualTransformation = if (isVisible) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        }
     )
 }
