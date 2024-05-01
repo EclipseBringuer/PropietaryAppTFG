46e7e5ddbdd2658e342cbc86d64449bd71e9f792
gabri <gabrielrl2004@gmail.com>
2024-05-01 20:09:54 +0200

    TrackingScreen preparada

---
+++ app/src/main/java/com/grl/clientapptfg/ui/screens/tracking/TrackingViewModel.kt
@@ -0,0 +1,9 @@
+package com.grl.clientapptfg.ui.screens.tracking
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TrackingViewModel @Inject constructor() : ViewModel() {
+}
