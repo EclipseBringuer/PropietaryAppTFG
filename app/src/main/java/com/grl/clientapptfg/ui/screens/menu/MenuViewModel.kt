ae0e7f2b9cc660e6fde4ed295f87b4f1fa4546cc
gabri <gabrielrl2004@gmail.com>
2024-05-01 20:37:20 +0200

    repositorio de producto implementado en el menu

--- app/src/main/java/com/grl/clientapptfg/ui/screens/menu/MenuViewModel.kt
+++ app/src/main/java/com/grl/clientapptfg/ui/screens/menu/MenuViewModel.kt
@@ -1,9 +1,11 @@
 package com.grl.clientapptfg.ui.screens.menu
 
 import androidx.lifecycle.ViewModel
+import com.grl.clientapptfg.data.repositories.ProductRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MenuViewModel @Inject constructor():ViewModel() {
+class MenuViewModel @Inject constructor(private val productRepository: ProductRepository) :
+    ViewModel() {
 }
