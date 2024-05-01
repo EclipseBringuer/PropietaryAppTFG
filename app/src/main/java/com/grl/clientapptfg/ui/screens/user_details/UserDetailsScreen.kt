981caf04becae0ad69ed816bbd452ba300f74bcb
gabri <gabrielrl2004@gmail.com>
2024-05-01 17:31:48 +0200

    UserDetails preparado

---
+++ app/src/main/java/com/grl/clientapptfg/ui/screens/user_details/UserDetailsScreen.kt
@@ -0,0 +1,15 @@
+package com.grl.clientapptfg.ui.screens.user_details
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.grl.clientapptfg.ui.theme.granate
+
+@Composable
+fun UserDetailsScreen(userDetailsViewModel: UserDetailsViewModel) {
+    ConstraintLayout(Modifier.fillMaxSize().background(granate)) {
+
+    }
+}
---
+++ app/src/main/java/com/grl/clientapptfg/ui/screens/user_details/UserDetailsViewModel.kt
@@ -0,0 +1,9 @@
+package com.grl.clientapptfg.ui.screens.user_details
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class UserDetailsViewModel @Inject constructor():ViewModel() {
+}
