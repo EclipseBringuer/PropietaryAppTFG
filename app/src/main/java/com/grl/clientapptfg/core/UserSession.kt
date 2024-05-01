f88acb2c8abafe5c6e64c4eb5da0ef7ac0081ba6
gabri <gabrielrl2004@gmail.com>
2024-05-01 20:12:28 +0200

    Eliminacion de variable innecesaria

--- app/src/main/java/com/grl/clientapptfg/core/UserSession.kt
+++ app/src/main/java/com/grl/clientapptfg/core/UserSession.kt
@@ -1,16 +1,10 @@
 package com.grl.clientapptfg.core
 
-import com.grl.clientapptfg.data.models.Item
 import com.grl.clientapptfg.data.models.UserModel
 
 class UserSession {
     companion object{
-        private var listOfItems:List<Item> = mutableListOf()
         private var user:UserModel? = null
-        fun addItem(item: Item){
-
-        }
-
         fun setUser(user:UserModel){
             this.user = user
         }
