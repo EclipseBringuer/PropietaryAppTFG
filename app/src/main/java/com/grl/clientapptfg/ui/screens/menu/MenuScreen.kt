e41a13d19e5ea272381d9491946cff8ba0e65e73
gabri <gabrielrl2004@gmail.com>
2024-05-01 16:51:40 +0200

    Pantalla de la Carta preparada

---
+++ app/src/main/java/com/grl/clientapptfg/ui/screens/menu/MenuScreen.kt
@@ -0,0 +1,15 @@
+package com.grl.clientapptfg.ui.screens.menu
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.grl.clientapptfg.ui.theme.granate
+
+@Composable
+fun MenuScreen(){
+    ConstraintLayout(Modifier.fillMaxSize().background(granate)) {
+    }
+}
---
+++ app/src/main/java/com/grl/clientapptfg/ui/screens/menu/MenuViewModel.kt
@@ -0,0 +1,7 @@
+package com.grl.clientapptfg.ui.screens.menu
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class MenuViewModel @Inject constructor():ViewModel() {
+}
