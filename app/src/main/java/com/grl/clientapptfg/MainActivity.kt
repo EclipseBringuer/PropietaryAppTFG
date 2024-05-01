f1a5c34ff9a197fec29cc3d535e9f62d305cfed0
gabri <gabrielrl2004@gmail.com>
2024-05-01 20:13:30 +0200

    MainActivity preparada

--- app/src/main/java/com/grl/clientapptfg/MainActivity.kt
+++ app/src/main/java/com/grl/clientapptfg/MainActivity.kt
@@ -8,10 +8,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.grl.clientapptfg.ui.screens.create_account.CreateAccountViewModel
 import com.grl.clientapptfg.ui.screens.login.LoginViewModel
+import com.grl.clientapptfg.ui.screens.menu.MenuViewModel
+import com.grl.clientapptfg.ui.screens.order.OrderViewModel
 import com.grl.clientapptfg.ui.screens.profile.ProfileViewModel
+import com.grl.clientapptfg.ui.screens.start.StartViewModel
 import com.grl.clientapptfg.ui.screens.tabs_menu.TabsMenuScreen
 import com.grl.clientapptfg.ui.screens.tabs_menu.TabsMenuViewModel
+import com.grl.clientapptfg.ui.screens.tracking.TrackingViewModel
+import com.grl.clientapptfg.ui.screens.user_details.UserDetailsViewModel
 import com.grl.clientapptfg.ui.theme.ClientAppTFGTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,6 +27,12 @@ class MainActivity : ComponentActivity() {
     private val tabsMenuViewModel: TabsMenuViewModel by viewModels()
     private val loginViewModel: LoginViewModel by viewModels()
     private val profileViewModel: ProfileViewModel by viewModels()
+    private val menuViewModel: MenuViewModel by viewModels()
+    private val userDetailsViewModel: UserDetailsViewModel by viewModels()
+    private val createAccountViewModel: CreateAccountViewModel by viewModels()
+    private val orderViewModel:OrderViewModel by viewModels()
+    private val startViewModel:StartViewModel by viewModels()
+    private val trackingViewModel:TrackingViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +46,13 @@ class MainActivity : ComponentActivity() {
                     TabsMenuScreen(
                         tabsMenuViewModel = tabsMenuViewModel,
                         loginViewModel = loginViewModel,
-                        profileViewModel = profileViewModel
+                        profileViewModel = profileViewModel,
+                        menuViewModel = menuViewModel,
+                        userDetailsViewModel = userDetailsViewModel,
+                        createAccountViewModel = createAccountViewModel,
+                        startViewModel = startViewModel,
+                        orderViewModel = orderViewModel,
+                        trackingViewModel = trackingViewModel
                     )
                 }
             }
