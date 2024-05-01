ba0975184e159e9cf149a36dd8c0d970fc20358d
gabri <gabrielrl2004@gmail.com>
2024-05-01 20:13:16 +0200

    MenuScreen preparada

--- app/src/main/java/com/grl/clientapptfg/ui/screens/menu/MenuScreen.kt
+++ app/src/main/java/com/grl/clientapptfg/ui/screens/menu/MenuScreen.kt
@@ -2,14 +2,24 @@ package com.grl.clientapptfg.ui.screens.menu
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.grl.clientapptfg.ui.theme.granate
 
 @Composable
-fun MenuScreen(){
+fun MenuScreen(menuViewModel: MenuViewModel) {
     ConstraintLayout(Modifier.fillMaxSize().background(granate)) {
+        val (texto, cabecera, cuerpo) = createRefs()
+        Text(
+            text = "Esto es la pantalla de carta",
+            Modifier
+                .constrainAs(texto) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                })
     }
 }
--- app/src/main/java/com/grl/clientapptfg/ui/screens/menu/MenuViewModel.kt
+++ app/src/main/java/com/grl/clientapptfg/ui/screens/menu/MenuViewModel.kt
@@ -1,7 +1,9 @@
 package com.grl.clientapptfg.ui.screens.menu
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class MenuViewModel @Inject constructor():ViewModel() {
 }
