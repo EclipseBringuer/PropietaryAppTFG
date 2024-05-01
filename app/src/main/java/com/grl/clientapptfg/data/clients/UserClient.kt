8f0e1a5cceca4ffdbd091a98ecd9353ba7f5ca66
gabri <gabrielrl2004@gmail.com>
2024-05-01 19:07:21 +0200

    Metodo en el cliente para crear usuario

--- app/src/main/java/com/grl/clientapptfg/data/clients/UserClient.kt
+++ app/src/main/java/com/grl/clientapptfg/data/clients/UserClient.kt
@@ -2,13 +2,22 @@ package com.grl.clientapptfg.data.clients
 
 import com.grl.clientapptfg.data.models.UserModel
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
-interface LoginClient {
+interface UserClient {
     @GET("user/{gmail}/{password}")
     suspend fun doLogin(
         @Path("gmail") gmail: String,
         @Path("password") password: String
+    ): Response<UserModel>
+
+    @POST("user/new")
+    suspend fun createUser(
+        @Query("token") token: String,
+        @Body newUser: UserModel
     ): Response<UserModel>
 }
