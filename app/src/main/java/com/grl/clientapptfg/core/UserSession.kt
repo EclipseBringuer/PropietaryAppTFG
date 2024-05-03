7c8a60b623b22e0ba38e0f908fee1718d0d8fb28
gabri <gabrielrl2004@gmail.com>
2024-05-03 18:19:01 +0200

    Método para recuperar al usuario en sesión

--- app/src/main/java/com/grl/clientapptfg/core/UserSession.kt
+++ app/src/main/java/com/grl/clientapptfg/core/UserSession.kt
@@ -3,10 +3,14 @@ package com.grl.clientapptfg.core
 import com.grl.clientapptfg.data.models.UserModel
 
 class UserSession {
-    companion object{
-        private var user:UserModel? = null
-        fun setUser(user:UserModel){
+    companion object {
+        private var user: UserModel? = null
+        fun setUser(user: UserModel) {
             this.user = user
+        }
+
+        fun getUser(): UserModel? {
+            return this.user
         }
     }
 }
