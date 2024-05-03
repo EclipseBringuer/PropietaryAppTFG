9bc431f4ffa6bdbd0359b74c2d2996a09a81f236
gabri <gabrielrl2004@gmail.com>
2024-05-03 17:09:46 +0200

    Nuevo método de sefuridad para encriptar las contraseñas y comprobarlas

--- app/src/main/java/com/grl/clientapptfg/utils/Util.kt
+++ app/src/main/java/com/grl/clientapptfg/utils/Util.kt
@@ -3,15 +3,24 @@ package com.grl.clientapptfg.utils
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
 import com.grl.clientapptfg.R
+import java.security.MessageDigest
 
 class Util {
     companion object {
         @Composable
         fun loadFontFamilyFromAssets(): FontFamily {
             return FontFamily(Font(R.font.aladin_regular))
+        }
+
+        fun hashPassword(password: String): String {
+            val bytes = MessageDigest.getInstance("SHA-256").digest(password.toByteArray())
+            return bytes.joinToString("") { "%02x".format(it) }
+        }
+
+        fun verifyPassword(password: String, hashedPassword: String): Boolean {
+            val newPasswordHash = hashPassword(password)
+            return newPasswordHash == hashedPassword
         }
     }
 }
