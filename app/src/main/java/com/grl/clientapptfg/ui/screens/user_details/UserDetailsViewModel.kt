6fae52d297e233790f2b14b560151de4afdb88af
gabri <gabrielrl2004@gmail.com>
2024-05-04 15:07:14 +0200

    Pantalla con los detalles del usuario terminado

--- app/src/main/java/com/grl/clientapptfg/ui/screens/user_details/UserDetailsViewModel.kt
+++ app/src/main/java/com/grl/clientapptfg/ui/screens/user_details/UserDetailsViewModel.kt
@@ -1,9 +1,17 @@
 package com.grl.clientapptfg.ui.screens.user_details
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class UserDetailsViewModel @Inject constructor() : ViewModel() {
+    private val _closeSession = MutableLiveData<Boolean>()
+    val closeSession: LiveData<Boolean> = _closeSession
+
+    fun changeCloseSession(boolean: Boolean){
+        _closeSession.value = boolean
+    }
 }
