1da509487a31b491e177b7db13f7fc67d026dca2
gabri <gabrielrl2004@gmail.com>
2024-05-04 15:06:26 +0200

    Dialogo con dos posibles opcines añadido

--- app/src/main/java/com/grl/clientapptfg/ui/components/AlertDialogs.kt
+++ app/src/main/java/com/grl/clientapptfg/ui/components/AlertDialogs.kt
@@ -8,10 +8,12 @@ import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.grl.clientapptfg.ui.theme.black
 import com.grl.clientapptfg.ui.theme.blackSoft
+import com.grl.clientapptfg.ui.theme.darkRed
 import com.grl.clientapptfg.ui.theme.granate
 import com.grl.clientapptfg.ui.theme.mostaza
 import com.grl.clientapptfg.ui.theme.mostazaSoft
@@ -55,6 +57,70 @@ fun ConfirmationDialog(onClick: () -> Unit, title: String, text: String) {
                 )
             ) {
                 Text(text = "Aceptar", color = black, fontFamily = aladinFont, fontSize = 20.sp)
+            }
+        }
+    )
+}
+
+@Composable
+fun ConfirmationDialogWithNegative(
+    onPositive: () -> Unit,
+    title: String,
+    text: String,
+    onNegative: () -> Unit
+) {
+    val aladinFont = Util.loadFontFamilyFromAssets()
+    AlertDialog(
+        onDismissRequest = {
+        },
+        containerColor = granate,
+        title = {
+            Text(
+                text = title,
+                color = white,
+                fontFamily = aladinFont,
+                fontSize = 33.sp,
+                textAlign = TextAlign.Center
+            )
+        },
+        text = {
+            Text(
+                text = text,
+                color = mostaza,
+                fontFamily = aladinFont,
+                fontSize = 20.sp,
+            )
+        },
+        confirmButton = {
+            Button(
+                modifier = Modifier
+                    .height(60.dp)
+                    .fillMaxWidth(),
+                onClick = onPositive,
+                colors = ButtonColors(
+                    contentColor = black,
+                    containerColor = mostaza,
+                    disabledContainerColor = mostazaSoft,
+                    disabledContentColor = blackSoft
+                )
+            ) {
+                Text(text = "Aceptar", color = black, fontFamily = aladinFont, fontSize = 20.sp)
+            }
+        },
+        dismissButton = {
+            Button(
+                modifier = Modifier
+                    .height(60.dp)
+                    .fillMaxWidth(),
+                onClick = onNegative,
+                colors = ButtonColors(
+                    contentColor = black,
+                    containerColor = darkRed,
+                    disabledContainerColor = mostazaSoft,
+                    disabledContentColor = blackSoft
+                )
+            ) {
+                Text(text = "Cancelar", color = black, fontFamily = aladinFont, fontSize = 20.sp)
             }
         }
     )
