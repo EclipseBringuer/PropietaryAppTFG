ca47d6c480df896ca29f4e789b672dac0ac8dcef
gabri <gabrielrl2004@gmail.com>
2024-05-05 18:53:18 +0200

    Modelo del producto terminado

--- app/src/main/java/com/grl/clientapptfg/data/models/ProductModel.kt
+++ app/src/main/java/com/grl/clientapptfg/data/models/ProductModel.kt
@@ -1,3 +1,10 @@
 package com.grl.clientapptfg.data.models
 
-data class ProductModel(var id: Int)
+data class ProductModel(
+    var id: Int,
+    var name: String,
+    var price: Double,
+    var photo: ByteArray,
+    var description: String,
+    var category: String
+)
