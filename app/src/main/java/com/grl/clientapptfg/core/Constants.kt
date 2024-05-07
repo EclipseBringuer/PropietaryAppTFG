356f185d3bfb0abdc018981315fad3f8f9f9aecb
gabri <gabrielrl2004@gmail.com>
2024-05-07 21:42:19 +0200

    Nuevas constantes

--- app/src/main/java/com/grl/clientapptfg/core/Constants.kt
+++ app/src/main/java/com/grl/clientapptfg/core/Constants.kt
@@ -1,7 +1,23 @@
 package com.grl.clientapptfg.core
 
 class Constants {
-    companion object{
+    companion object {
         const val TOKEN = "15112004"
+
+        class Category {
+            companion object {
+                const val KEBAB = "Kebab"
+                const val DURUM = "Dürüm"
+                const val MENU = "Menús"
+                const val RACION = "Raciones"
+                const val HAMBURGUESA = "Hamburguesa"
+                const val LAHMACUN = "Lahmacun"
+                const val BEBIDA = "Bebidas"
+
+                fun getListOfCategories(): List<String> {
+                    return listOf(KEBAB, DURUM, HAMBURGUESA, BEBIDA, LAHMACUN, RACION, MENU)
+                }
+            }
+        }
     }
 }
