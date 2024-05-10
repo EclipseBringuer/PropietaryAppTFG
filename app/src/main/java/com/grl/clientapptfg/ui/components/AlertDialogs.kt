9f45da68a3f9de71b1f5615fccd9dc831719c9e9
gabri <gabrielrl2004@gmail.com>
2024-05-11 00:10:31 +0200

    Parametro eliminado

--- app/src/main/java/com/grl/clientapptfg/ui/components/AlertDialogs.kt
+++ app/src/main/java/com/grl/clientapptfg/ui/components/AlertDialogs.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.HorizontalDivider
@@ -17,7 +16,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -256,7 +254,6 @@ fun ConfirmationDialogWithNegative(
 fun OrderProductDialog(
     onDismiss: () -> Unit,
     product: ProductModel,
-    painter: Painter,
     context: Context,
     notLogged: () -> Unit
 ) {
