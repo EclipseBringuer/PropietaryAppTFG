c87b2c642d504f1323bdf1f090e9c7e486206f8c
gabri <gabrielrl2004@gmail.com>
2024-05-13 20:22:58 +0200

    Eliminacion de la lista de productos en sesion

--- app/src/main/java/com/grl/clientapptfg/core/UserSession.kt
+++ app/src/main/java/com/grl/clientapptfg/core/UserSession.kt
@@ -1,30 +1,17 @@
 package com.grl.clientapptfg.core
 
-import com.grl.clientapptfg.data.models.ProductModel
 import com.grl.clientapptfg.data.models.UserModel
 
 class UserSession {
     companion object {
         private var user: UserModel? = null
-        private var productList: MutableList<ProductModel> = mutableListOf()
+
         fun setUser(user: UserModel?) {
             this.user = user
         }
 
         fun getUser(): UserModel? {
             return this.user
-        }
-
-        fun addProduct(product:ProductModel){
-            productList.add(product)
-        }
-
-        fun removeProduct(product: ProductModel){
-            productList.remove(product)
-        }
-
-        fun cleanList(){
-            productList = mutableListOf()
         }
     }
 }
