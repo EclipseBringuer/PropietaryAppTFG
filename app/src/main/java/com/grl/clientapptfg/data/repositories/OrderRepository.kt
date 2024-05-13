1f3cf7705c1e506480747f1e9391cbd5cd236903
gabri <gabrielrl2004@gmail.com>
2024-05-13 20:22:00 +0200

    Repositorio de Order

---
+++ app/src/main/java/com/grl/clientapptfg/data/repositories/OrderRepository.kt
@@ -0,0 +1,11 @@
+package com.grl.clientapptfg.data.repositories
+
+import com.grl.clientapptfg.data.models.OrderModel
+import com.grl.clientapptfg.data.services.OrderService
+import javax.inject.Inject
+
+class OrderRepository @Inject constructor(private val api: OrderService) {
+    suspend fun createNewOrder(newOrder: OrderModel): OrderModel {
+        return api.createNewOrder(newOrder)
+    }
+}
