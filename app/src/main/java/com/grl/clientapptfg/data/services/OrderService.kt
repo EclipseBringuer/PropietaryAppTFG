a90ed99e038b899adf5f3dca7b7f7c8adcdaaa62
gabri <gabrielrl2004@gmail.com>
2024-05-13 20:22:10 +0200

    Servicio de Order

---
+++ app/src/main/java/com/grl/clientapptfg/data/services/OrderService.kt
@@ -0,0 +1,17 @@
+package com.grl.clientapptfg.data.services
+
+import com.grl.clientapptfg.core.Constants
+import com.grl.clientapptfg.data.clients.OrderClient
+import com.grl.clientapptfg.data.models.OrderModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class OrderService @Inject constructor(private val orderClient: OrderClient) {
+    suspend fun createNewOrder(newOrder: OrderModel): OrderModel {
+        return withContext(Dispatchers.IO) {
+            val response = orderClient.createOrder(Constants.TOKEN, newOrder)
+            response.body()!!
+        }
+    }
+}
