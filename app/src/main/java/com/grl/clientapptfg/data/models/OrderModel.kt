75e3d1cc4d0040a17314c0d3bb16f37413c79efc
gabri <gabrielrl2004@gmail.com>
2024-05-13 20:21:46 +0200

    Modelo de Order

---
+++ app/src/main/java/com/grl/clientapptfg/data/models/OrderModel.kt
@@ -0,0 +1,9 @@
+package com.grl.clientapptfg.data.models
+
+data class OrderModel(
+    var id: Int = 0,
+    var price: Double,
+    var paymentMethod: String,
+    var items: List<ItemModel>,
+    var user: UserModel
+)
