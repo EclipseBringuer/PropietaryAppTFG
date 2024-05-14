2f369c7a885c23a2a836d67c5634004a8c847c5d
gabri <gabrielrl2004@gmail.com>
2024-05-14 18:14:56 +0200

    Alert dialog de selección terminado

--- app/src/main/java/com/grl/clientapptfg/ui/components/AlertDialogs.kt
+++ app/src/main/java/com/grl/clientapptfg/ui/components/AlertDialogs.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,8 +28,10 @@ import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import com.grl.clientapptfg.core.Constants
 import com.grl.clientapptfg.core.UserSession
 import com.grl.clientapptfg.data.models.ProductModel
+import com.grl.clientapptfg.ui.screens.order.OrderViewModel
 import com.grl.clientapptfg.ui.screens.tabs_menu.TabsMenuViewModel
 import com.grl.clientapptfg.ui.theme.black
 import com.grl.clientapptfg.ui.theme.blackSoft
@@ -175,12 +180,15 @@ fun ConfirmationDialogWithNegative(
                 fontFamily = aladinFont,
                 fontSize = 30.sp,
                 textAlign = TextAlign.Center,
-                modifier = Modifier.constrainAs(description) {
-                    top.linkTo(titleText.bottom)
-                    end.linkTo(parent.end)
-                    start.linkTo(parent.start)
-                    bottom.linkTo(bConfirm.top)
-                }
+                style = TextStyle(lineHeight = 40.sp),
+                modifier = Modifier
+                    .padding(horizontal = 10.dp)
+                    .constrainAs(description) {
+                        top.linkTo(titleText.bottom)
+                        end.linkTo(parent.end)
+                        start.linkTo(parent.start)
+                        bottom.linkTo(bConfirm.top)
+                    }
             )
             Button(
                 modifier = Modifier
@@ -353,6 +361,219 @@ fun OrderProductDialog(
                     text = "Añadir",
                     fontFamily = Util.loadFontFamilyFromAssets(),
                     fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun OrderOptionsDialog(
+    paymentValue: String = "",
+    deliveryValue: String = "",
+    orderViewModel: OrderViewModel,
+    total: Double,
+    onConfirm: () -> Unit,
+) {
+    val aladinFont = Util.loadFontFamilyFromAssets()
+    Dialog(onDismissRequest = { }) {
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(580.dp)
+                .background(granate, RoundedCornerShape(20.dp))
+        ) {
+            val (spacerSecond, spacerFirst, title, divider, deliveryText, paymentText, button, radioCard, radioCash, radioDeliver, radioPick, textCard, textCash, textDeliver, textPick) = createRefs()
+            Text(
+                text = "Opciones del Pedido",
+                fontFamily = aladinFont,
+                fontWeight = FontWeight.Bold,
+                color = mostaza,
+                fontSize = 50.sp,
+                textAlign = TextAlign.Center,
+                style = TextStyle(lineHeight = 50.sp),
+                modifier = Modifier.constrainAs(title) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+            HorizontalDivider(
+                Modifier
+                    .constrainAs(divider) {
+                        top.linkTo(title.bottom)
+                    }
+                    .padding(horizontal = 20.dp)
+                    .padding(bottom = 30.dp),
+                color = mostaza,
+                thickness = 3.dp
+            )
+            Text(
+                text = "Entrega:",
+                fontFamily = aladinFont,
+                fontWeight = FontWeight.Bold,
+                color = mostaza,
+                fontSize = 35.sp,
+                modifier = Modifier
+                    .padding(bottom = 20.dp)
+                    .constrainAs(deliveryText) {
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        top.linkTo(divider.bottom)
+                    }
+            )
+            RadioButton(
+                selected = deliveryValue == Constants.DELIVERY,
+                onClick = { orderViewModel.changeDeliveryValue(Constants.DELIVERY) },
+                colors = RadioButtonColors(
+                    selectedColor = mostaza,
+                    unselectedColor = white,
+                    disabledSelectedColor = blackSoft,
+                    disabledUnselectedColor = blackSoft
+                ),
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .size(20.dp)
+                    .constrainAs(radioDeliver) {
+                        top.linkTo(textDeliver.top)
+                        start.linkTo(parent.start)
+                        bottom.linkTo(textDeliver.bottom)
+                    }
+            )
+            Text(text = "A Domicilio (${Util.formatDouble(total)}€ + 1.50€)",
+                fontFamily = aladinFont,
+                fontSize = 25.sp,
+                color = white,
+                modifier = Modifier.constrainAs(textDeliver) {
+                    start.linkTo(radioDeliver.end)
+                    top.linkTo(deliveryText.bottom)
+                })
+            Spacer(modifier = Modifier
+                .height(20.dp)
+                .constrainAs(spacerFirst) {
+                    top.linkTo(textDeliver.bottom)
+                    bottom.linkTo(textPick.top)
+                })
+            RadioButton(
+                selected = deliveryValue == Constants.PICK,
+                onClick = { orderViewModel.changeDeliveryValue(Constants.PICK) },
+                colors = RadioButtonColors(
+                    selectedColor = mostaza,
+                    unselectedColor = white,
+                    disabledSelectedColor = blackSoft,
+                    disabledUnselectedColor = blackSoft
+                ),
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .size(20.dp)
+                    .constrainAs(radioPick) {
+                        start.linkTo(parent.start)
+                        top.linkTo(textPick.top)
+                        bottom.linkTo(textPick.bottom)
+                    }
+            )
+            Text(text = "Establecimiento",
+                fontFamily = aladinFont,
+                color = white,
+                fontSize = 25.sp,
+                modifier = Modifier.constrainAs(textPick) {
+                    top.linkTo(spacerFirst.bottom)
+                    start.linkTo(radioPick.end)
+                })
+            Text(text = "Método de Pago:", fontFamily = aladinFont,
+                fontWeight = FontWeight.Bold,
+                color = mostaza,
+                fontSize = 35.sp,
+                modifier = Modifier
+                    .padding(vertical = 20.dp)
+                    .constrainAs(paymentText) {
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        top.linkTo(textPick.bottom)
+                    })
+            Text(text = "Tarjeta",
+                fontFamily = aladinFont,
+                color = white,
+                fontSize = 25.sp,
+                modifier = Modifier.constrainAs(textCard) {
+                    top.linkTo(paymentText.bottom)
+                    start.linkTo(radioPick.end)
+                })
+            RadioButton(
+                selected = paymentValue == Constants.CARD,
+                onClick = { orderViewModel.changePaymentValue(Constants.CARD) },
+                colors = RadioButtonColors(
+                    selectedColor = mostaza,
+                    unselectedColor = white,
+                    disabledSelectedColor = blackSoft,
+                    disabledUnselectedColor = blackSoft
+                ),
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .size(20.dp)
+                    .constrainAs(radioCard) {
+                        top.linkTo(textCard.top)
+                        bottom.linkTo(textCard.bottom)
+                        start.linkTo(parent.start)
+                    }
+            )
+            Spacer(modifier = Modifier
+                .height(20.dp)
+                .constrainAs(spacerSecond) {
+                    top.linkTo(textCard.bottom)
+                    bottom.linkTo(textCash.top)
+                })
+            Text(text = "Efectivo", fontFamily = aladinFont,
+                color = white,
+                fontSize = 25.sp,
+                modifier = Modifier.constrainAs(textCash) {
+                    top.linkTo(spacerSecond.bottom)
+                    start.linkTo(radioCash.end)
+                })
+            RadioButton(
+                selected = paymentValue == Constants.CASH,
+                onClick = { orderViewModel.changePaymentValue(Constants.CASH) },
+                colors = RadioButtonColors(
+                    selectedColor = mostaza,
+                    unselectedColor = white,
+                    disabledSelectedColor = blackSoft,
+                    disabledUnselectedColor = blackSoft
+                ),
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .size(20.dp)
+                    .constrainAs(radioCash) {
+                        start.linkTo(parent.start)
+                        top.linkTo(textCash.top)
+                        bottom.linkTo(textCash.bottom)
+                    }
+            )
+            Button(
+                onClick = { onConfirm() },
+                Modifier
+                    .height(70.dp)
+                    .fillMaxWidth()
+                    .constrainAs(button) {
+                        bottom.linkTo(parent.bottom)
+                    },
+                shape = RoundedCornerShape(
+                    bottomEnd = 20.dp,
+                    bottomStart = 20.dp,
+                    topStart = 0.dp,
+                    topEnd = 0.dp
+                ),
+                colors = ButtonColors(
+                    contentColor = black,
+                    containerColor = mostaza,
+                    disabledContainerColor = mostazaSoft,
+                    disabledContentColor = blackSoft
+                )
+            ) {
+                Text(
+                    text = "Confirmar",
+                    fontFamily = Util.loadFontFamilyFromAssets(),
+                    fontSize = 40.sp,
                     fontWeight = FontWeight.Bold
                 )
             }
