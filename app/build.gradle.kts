6be09818fcb90ceb7df4c3d6a8cc3923cdeda1ba
gabri <gabrielrl2004@gmail.com>
2024-05-23 23:46:54 +0200

    Cambio de la id de proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 }
 
 android {
-    namespace = "com.grl.clientapptfg"
+    namespace = "com.grl.propietaryapptfg"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.grl.clientapptfg"
+        applicationId = "com.grl.propietaryapptfg"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
