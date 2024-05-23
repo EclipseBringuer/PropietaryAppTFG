15463cf8b667ad0c6bf7d7af53404d23a241593f
gabri <gabrielrl2004@gmail.com>
2024-05-23 23:49:36 +0200

    Nueva entradac en la MainActivity

--- app/src/main/java/com/grl/propietaryapptfg/MainActivity.kt
+++ app/src/main/java/com/grl/propietaryapptfg/MainActivity.kt
@@ -8,48 +8,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.grl.propietaryapptfg.ui.screens.create_account.CreateAccountViewModel
-import com.grl.propietaryapptfg.ui.screens.login.LoginViewModel
-import com.grl.propietaryapptfg.ui.screens.menu.MenuViewModel
-import com.grl.propietaryapptfg.ui.screens.order.OrderViewModel
-import com.grl.propietaryapptfg.ui.screens.profile.ProfileViewModel
-import com.grl.propietaryapptfg.ui.screens.tabs_menu.TabsMenuScreen
-import com.grl.propietaryapptfg.ui.screens.tabs_menu.TabsMenuViewModel
-import com.grl.propietaryapptfg.ui.screens.tracking.TrackingViewModel
-import com.grl.propietaryapptfg.ui.screens.user_details.UserDetailsViewModel
+import com.grl.propietaryapptfg.ui.screens.order_details.OrderDetailsViewModel
+import com.grl.propietaryapptfg.ui.screens.order_list.OrderListViewModel
+import com.grl.propietaryapptfg.ui.screens.principal.PrincipalScreen
+import com.grl.propietaryapptfg.ui.screens.principal.PrincipalViewModel
 import com.grl.propietaryapptfg.ui.theme.ClientAppTFGTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private val tabsMenuViewModel: TabsMenuViewModel by viewModels()
-    private val loginViewModel: LoginViewModel by viewModels()
-    private val profileViewModel: ProfileViewModel by viewModels()
-    private val menuViewModel: MenuViewModel by viewModels()
-    private val userDetailsViewModel: UserDetailsViewModel by viewModels()
-    private val createAccountViewModel: CreateAccountViewModel by viewModels()
-    private val orderViewModel:OrderViewModel by viewModels()
-    private val trackingViewModel:TrackingViewModel by viewModels()
-
+    private val principalViewModel: PrincipalViewModel by viewModels()
+    private val orderDetailsViewModel: OrderDetailsViewModel by viewModels()
+    private val orderListViewModel: OrderListViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ClientAppTFGTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TabsMenuScreen(
-                        tabsMenuViewModel = tabsMenuViewModel,
-                        loginViewModel = loginViewModel,
-                        profileViewModel = profileViewModel,
-                        menuViewModel = menuViewModel,
-                        userDetailsViewModel = userDetailsViewModel,
-                        createAccountViewModel = createAccountViewModel,
-                        orderViewModel = orderViewModel,
-                        trackingViewModel = trackingViewModel
+                    PrincipalScreen(
+                        principalViewModel = principalViewModel,
+                        orderListViewModel = orderListViewModel,
+                        orderDetailsViewModel = orderDetailsViewModel
                     )
                 }
             }
