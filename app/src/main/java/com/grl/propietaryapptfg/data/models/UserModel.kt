bebbd504e91f5e39e752a5adf1e7ff73d838993a
gabri <gabrielrl2004@gmail.com>
2024-05-23 23:48:45 +0200

    Adaptación del modelo de usuario

--- app/src/main/java/com/grl/propietaryapptfg/data/models/UserModel.kt
+++ app/src/main/java/com/grl/propietaryapptfg/data/models/UserModel.kt
@@ -1,10 +1,7 @@
 package com.grl.propietaryapptfg.data.models
 
 data class UserModel(
-    var id: Int = 0,
     var name: String,
-    var gmail: String,
-    var password: String,
-    var phone: String = "Vacio",
-    var address: String = "Vacio"
+    var phone: String,
+    var address: String
 )
