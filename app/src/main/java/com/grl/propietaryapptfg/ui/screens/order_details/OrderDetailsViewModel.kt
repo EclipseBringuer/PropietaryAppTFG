6497db1dd87c7f6b89aeb47f825b6fdddba4e881
gabri <gabrielrl2004@gmail.com>
2024-05-24 12:39:07 +0200

    Eliminacion de elementos innecesarios

--- app/src/main/java/com/grl/propietaryapptfg/ui/screens/order_details/OrderDetailsViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.grl.propietaryapptfg.ui.screens.order_details
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class OrderDetailsViewModel @Inject constructor() : ViewModel() {
-}
