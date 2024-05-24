f122a5bbd911aea9be389d90f917fe3a936a4cee
gabri <gabrielrl2004@gmail.com>
2024-05-24 17:53:44 +0200

    Eliminacion de métodos innecesareos en OrderClient

--- app/src/main/java/com/grl/propietaryapptfg/data/clients/OrderClient.kt
+++ app/src/main/java/com/grl/propietaryapptfg/data/clients/OrderClient.kt
@@ -2,26 +2,12 @@ package com.grl.propietaryapptfg.data.clients
 
 import com.grl.propietaryapptfg.data.models.OrderModel
 import retrofit2.Response
-import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface OrderClient {
-    @POST("order/new")
-    suspend fun createOrder(
-        @Query("token") token: String,
-        @Body order: OrderModel
-    ): Response<OrderModel>
-
-    @GET("order/findByUser")
-    suspend fun getOrdersByUser(
-        @Query("token") token: String,
-        @Query("id") id: Int
-    ): Response<List<OrderModel>>
-
     @GET("order/getAllNotCompleted")
     suspend fun getAllNotCompleted(@Query("token") token: String): Response<List<OrderModel>>
 
